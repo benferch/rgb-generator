f99b79a48c04c8845b28993947785711058cddf3
Ben F <ben@realeaz.de>
2019-10-06 22:24:18 +0200

    only a box is colored - now also a hex code is shown - everything is put into a function which loads on page load - colors were changed - font added and changed

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -1,15 +1,24 @@
-var min = 0;
-var max = 255;
+window.onload = init();
 
-var r = Math.round(Math.random() * (max - min)) + min;
-var g = Math.round(Math.random() * (max - min)) + min;
-var b = Math.round(Math.random() * (max - min)) + min;
-var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+function init() {
+    var min = 0;
+    var max = 255;
 
-function refreshPage() {
-    window.location.reload();
-    return
+    var r = Math.round(Math.random() * (max - min)) + min;
+    var g = Math.round(Math.random() * (max - min)) + min;
+    var b = Math.round(Math.random() * (max - min)) + min;
+    var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    function rgbToHex(r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+
+    document.getElementById("rect").style.fill = rgb;
+    document.getElementById("rgb").innerHTML = rgb;
+    document.getElementById("hex").innerHTML = rgbToHex(r, g, b);
 }
-
-document.body.style.backgroundColor = rgb;
-document.getElementById("rgb").innerHTML = rgb;

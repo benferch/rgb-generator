b8c4ee6fa383e133dd3343947c919722e773f407
Ben Ferch <ben@benferch.de>
2020-10-03 22:09:02 +0200

    added ability to copy color code

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -1,24 +1,36 @@
-window.onload = init();
+window.onload = generateColor();
 
-function init() {
-    var min = 0;
-    var max = 255;
+function generateColor() {
+  var min = 0;
+  var max = 255;
 
-    var r = Math.round(Math.random() * (max - min)) + min;
-    var g = Math.round(Math.random() * (max - min)) + min;
-    var b = Math.round(Math.random() * (max - min)) + min;
-    var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+  var r = Math.round(Math.random() * (max - min)) + min;
+  var g = Math.round(Math.random() * (max - min)) + min;
+  var b = Math.round(Math.random() * (max - min)) + min;
+  var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
 
-    function componentToHex(c) {
-        var hex = c.toString(16);
-        return hex.length == 1 ? "0" + hex : hex;
-    }
+  function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
 
-    function rgbToHex(r, g, b) {
-        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-    }
+  function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  }
 
-    document.getElementById("rect").style.fill = rgb;
-    document.getElementById("rgb").innerHTML = rgb;
-    document.getElementById("hex").innerHTML = rgbToHex(r, g, b);
+  document.getElementById("rect").style.fill = rgb;
+  document.getElementById("rgb").innerHTML = rgb;
+  document.getElementById("hex").innerHTML = rgbToHex(r, g, b);
+}
+
+function copy(code) {
+  document.getElementById("hidden").type = "text";
+  document.getElementById("hidden").value = document.getElementById(
+    code
+  ).innerText;
+  document.getElementById("hidden").focus();
+  document.getElementById("hidden").select();
+  document.execCommand("copy");
+  window.getSelection().empty();
+  document.getElementById("hidden").type = "hidden";
 }
